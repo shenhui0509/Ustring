6bebb58cbbf5a42e49e4fc2aeee2d33b90faf5a5
shenhui0509 <shenhui0509@outlook.com>
2016-08-25 12:25:00 +0800

    Update pattern_search.cc

--- src/pattern_search.cc
+++ src/pattern_search.cc
@@ -1,4 +1,5 @@
 #include <alogrithm> // std::max
+#include <vector>
 #include "pattern_search.h"
 
 namespace ustr{
@@ -6,6 +7,8 @@ namespace ustr{
  * TODO : determine the threshold by benchmark
  */
 constexpr size_t threshold = 4096;
+using std::vector;
+
 
 bool PatternSearcher::Match(const string &text, const string &pattern)
 {
@@ -37,4 +40,83 @@ size_t PatternSearcher::RFind(const string &text, const string &pattern)
   return m - boyer_moore_internal(r_text, r_pattern, true) - 1;
 }
 
+static array<int, ASCII_NUM> compile_bad_char(const string &pattern)
+{
+  array<int, ASCII_NUM> bad_char;
+  bad_char.fill(-1);
+  auto len = pattern.size();
+  for(size_t i = 0; i < len; ++i){
+    bad_char[static_cast<int>(pattern[i])] = i;
+  }
+  return bad_char;
+}
+
+static vector<int> suffix(const string &pattern)
+{
+  auto len = pattern.length();
+  vector<int> suffix_(len, 0);
+  suffix_[len - 1] = len;
+
+  int f = 0, i = 0, g = len - 1;
+  for(i = len - 2; i >= 0; --i){
+    if(i > g && suffix_[i + len - 1 - f] < i - g)
+      suffix_[i] = suffix_[i + len - 1 - f];
+    else{
+      if(i < g)
+        g = i;
+      f = i;
+      while(g >= 0 && pattern[g] == pattern[g + len - 1 -f])
+        --g;
+      suffix_[i] = f - g;
+    }
+  }
+  return suffix_;
+}
+
+static vector<int> compile_good_suffix(const string &pattern)
+{
+  auto suffix_ = suffix(pattern);
+  auto len = pattern.size();
+  vector<int> good_suffix(len, 0);
+  int i = 0, j = 0;
+  std::fill(good_suffix.begin(), good_suffix.end(), len);
+
+  for(i = len - 1; i >= 0; --i){
+    if(suffix_[i] == i + 1){
+      for(; j < len - 1 - i; j++){
+        if(good_suffix[j] == len)
+          good_suffix[j] = len - 1 - i;
+      }
+    }
+  }
+
+  for(i = 0; i <= len - 2; ++i){
+    good_suffix[len - 1 - suffix_[i]] = len - 1 - i;
+  }
+  return good_suffix;
+}
+
+size_t boyer_moore_internal(const string &text, const string &pattern, bool match)
+{
+    auto bad_char = compile_bad_char(pattern);
+  auto good_suffix = compile_good_suffix(pattern);
+
+  auto n = text.length(), m = pattern.length();
+  size_t s = 0;
+  size_t result = 0;
+  while(s <= (n - m)){
+    int j = m - 1;
+    while(j >= 0 && pattern[j] == text[s + j])
+      --j;
+    if(j < 0){
+      if(match)
+        return s;
+      ++result;
+      s += good_suffix[0];
+    } else {
+      s += std::max(good_suffix[j], j - bad_char[static_cast<int>(text[s+j])]);
+    }
+  }
+  return match ? string::npos : result;
+}
 }//namespace ustr
